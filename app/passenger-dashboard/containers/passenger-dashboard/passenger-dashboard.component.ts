384303ec1b0a9560536b8b1c162758a44b5a0c8d
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-01 03:18:28 +0000

    Add onInit

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: "passenger-dashboard",
@@ -24,20 +25,26 @@ import { Passenger } from '../../models/passenger.interface';
         </div>
     `
 })
-export class PassengerDashboardComponent {
-    passengers: Passenger[] = [
-        {
-            id: 1,
-            fullname: "Ovidiu",
-            checkedIn: true,
-            checkedInDate: 1490742000000,
-            children: null
-        }, {
-            id: 2,
-            fullname: "Claudiu",
-            checkedIn: false,
-            checkedInDate: null,
-            children: []
-        }
-    ];
+export class PassengerDashboardComponent implements OnInit {
+    passengers: Passenger[];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.passengers = [
+            {
+                id: 1,
+                fullname: "Ovidiu",
+                checkedIn: true,
+                checkedInDate: 1490742000000,
+                children: null
+            }, {
+                id: 2,
+                fullname: "Claudiu",
+                checkedIn: false,
+                checkedInDate: null,
+                children: []
+            }
+        ];
+    }
 }
