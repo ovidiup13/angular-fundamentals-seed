2ebbe7b72dd82d75acf501883f81cd962b9b99da
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-08 03:14:25 +0000

    Add router module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,38 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
 //import a different module for server-side rendering
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
-import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home.component";
+import { NotFoundComponent } from "./not-found.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full" // match empty string
+  },
+  {
+    path: "**", // any routes that do not exist in the application
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     //angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     //custom modules
     PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
