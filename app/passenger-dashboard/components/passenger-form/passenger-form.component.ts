09bddc389e6b49b984d71af8a68b8e3b3334bb27
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-08 01:43:49 +0000

    Tie up form to service

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,18 +1,24 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, EventEmitter, Output } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
+import { Baggage } from "../../models/baggage.interface";
 
 @Component({
   selector: "passenger-form",
   styleUrls: ["passenger-form.component.scss"],
   template: `
-  <form #form="ngForm" novalidate>
+  <form (ngSubmit)="handleSubmit(form.value, form.valid)" #form="ngForm" novalidate>
             {{detail | json}}
             <div>
               Passenger name:
               <input
-              type="text"
-              name="fullname"
-              [ngModel]="detail?.fullname">
+                type="text"
+                name="fullname"
+                required
+                #fullName="ngModel"
+                [ngModel]="detail?.fullname">
+              <div *ngIf="fullName.errors?.required && fullName.dirty" class="error">
+                Passenger name is required.
+              </div>  
             </div>
 
             <div>
@@ -20,28 +26,21 @@ import { Passenger } from "../../models/passenger.interface";
               <input
               type="text"
               name="id"
+              required
+              #id="ngModel"
               [ngModel]="detail?.id">
+              <div *ngIf="id.errors?.required && id.dirty" class="error">
+                Passenger id is required.
+              </div> 
             </div>
 
             <div>
               <label>
                 <input
-                type="radio"
-                [value]="true"
+                type="checkbox"
                 name="checkedIn"
                 [ngModel]="detail?.checkedIn"
                 (ngModelChange)="toggleCheckedIn($event)">
-                Yes
-              </label>
-
-              <label>
-                <input
-                type="radio"
-                [value]="false"
-                name="checkedIn"
-                [ngModel]="detail?.checkedIn"
-                (ngModelChange)="toggleCheckedIn($event)">
-                No
               </label>
             </div>
 
@@ -53,18 +52,61 @@ import { Passenger } from "../../models/passenger.interface";
                   [ngModel]="detail?.checkedInDate">
             </div>
 
-            {{ form.value | json }}
+            <div>
+              Luggage:
+              <select
+                name="baggage"
+                [ngModel]="detail?.baggage">
+                <option
+                  *ngFor="let item of baggage"
+                  [value]="item.key"
+                  [selected]="item.key === detail?.baggage">
+                    {{ item.value }}
+                </option>
+              </select>
+            </div>
+
+            <button type="submit" [disabled]="form.invalid">
+              Update passenger
+            </button>
         </form>
     `
 })
 export class PassengerFormComponent {
   @Input() detail: Passenger;
 
+  @Output() update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+  baggage: Baggage[] = [
+    {
+      key: "none",
+      value: "No baggage"
+    },
+    {
+      key: "hand-only",
+      value: "Hand baggage"
+    },
+    {
+      key: "hold-only",
+      value: "Hold baggage"
+    },
+    {
+      key: "hand-hold",
+      value: "Hand and hold baggage"
+    }
+  ];
+
   constructor() {}
 
   toggleCheckedIn(checkedIn: boolean) {
     if (checkedIn) {
       this.detail.checkedInDate = Date.now();
+    }
+  }
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      this.update.emit(passenger);
     }
   }
 }
--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -10,6 +10,7 @@ import { Passenger } from "../../models/passenger.interface";
         <div>
           <passenger-form
             [detail]="passenger"
+            (update)="onUpdatePassenger($event)"
           >
           </passenger-form>
         </div>
@@ -24,5 +25,14 @@ export class PassengerViewerComponent implements OnInit {
     this.passengerService
       .getPassenger(1)
       .subscribe((data: Passenger) => (this.passenger = data));
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe(
+        (data: Passenger) =>
+          (this.passenger = Object.assign({}, this.passenger, event))
+      );
   }
 }
